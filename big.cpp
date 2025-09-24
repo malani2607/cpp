6398e1e6e8d6c8b381c7bc81ba3781dda039279e
malani2607 <hiralmalani574@gmail.com>
2025-09-24 11:33:37 +0530

    Add files via upload

---
+++ big.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << " H     H" << endl;
+    cout << " H     H" << endl;
+    cout << " H H H H" << endl;
+    cout << " H     H" << endl;
+    cout << " H     H" << endl;
+
+
+    return 0;
+}
+
