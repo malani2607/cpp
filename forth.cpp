d225ea75b88599ccf4381ea187f756bbc68d1bcb
malani2607 <hiralmalani574@gmail.com>
2025-09-24 11:36:54 +0530

    Add files via upload

---
+++ forth.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Name    : John Doe\n";
+    cout << "City    : Mumbai\n";
+    cout << "Address : 221B Baker Street\n";
+    cout << "Pincode : 400001\n";
+
+    return 0;
+}
+
